dc0ca16e610fcb606f1cdf1667d93c3ed7ac4be8
asaduzzaman <asaduzzaman@brainstation-23.com>
2023-04-04 00:41:21 +0600

    242 solved

--- week-1/242.ts
+++ week-1/242.ts
@@ -24,3 +24,18 @@ function isAnagram(s: string, t: string): boolean {
 
   return true;
 }
+
+// function isAnagram(s: string, t: string): boolean {
+//     if(s.length !== t.length) return false;
+//     let count = {};
+//     for(let char of s){
+//         count[char] = (count[char] || 0) + 1;
+//     }
+
+//     for(let char of t){
+//         if(!count[char]) return false;
+//         count[char] --;
+//     }
+
+//     return true;
+// };
